a92921873a0589df2b6fbf2c00fe2d0982115206
Ade Bateman <bateman@acm.org>
2022-11-14 16:47:12 -0800

    Add login fail test

--- test/login.test.ts
+++ test/login.test.ts
@@ -41,6 +41,38 @@ describe("login tests", function () {
     scope.done();
   });
 
+  it("invalid", async function () {
+    const email = "email@example.com"
+    const scope = nock("https://cloud.supernote.com")
+      .post("/api/official/user/query/random/code", (body) =>
+        body.countryCode == 93 && body.account == email
+      )
+      .reply(200, {
+        "success": true,
+        "errorCode": null,
+        "errorMsg": null,
+        "randomCode": "12345678",
+        "timestamp": 1234567890123
+      })
+      .post("/api/official/user/account/login/new")
+      .reply(200, {
+        "success": false,
+        "errorCode": null,
+        "errorMsg": null,
+        "token": null,
+        "counts": null,
+        "userName": null,
+        "avatarsUrl": null,
+        "lastUpdateTime": null,
+        "isBind": null,
+        "isBindEquipment": null,
+        "soldOutCount": 0
+      });
+    let token = await login(email, "00000000");
+    assert.equal(token, null);
+    scope.done();
+  });
+
   beforeEach(() => {
     nock.disableNetConnect();
   });
