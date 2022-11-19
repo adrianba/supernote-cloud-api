909b8f0b37440803b380b75ae3262a78cb1e315c
Ade Bateman <bateman@acm.org>
2022-11-19 14:52:44 -0800

    Add esbuild bundling.

---
+++ esbuild.config.cjs
@@ -0,0 +1,23 @@
+const esbuild = require("esbuild");
+
+// Automatically exclude all node_modules from the bundled version
+const { nodeExternalsPlugin } = require("esbuild-node-externals");
+
+// Find all .ts files - alternative is to just include ./src/index.ts and
+// esbuild will recursively find all dependencies from that single file
+const glob = require("glob");
+const entryPoints = glob.sync("./src/**/*.ts");
+
+const config = {
+  entryPoints,
+  outfile: "./lib/index.js",
+  bundle: true,
+  minify: true,
+  platform: "node",
+  sourcemap: true,
+  target: "node18",
+  tsconfig: "./tsconfig.build.json",
+  plugins: [nodeExternalsPlugin()],
+};
+
+esbuild.build(config).catch(() => process.exit(1));
